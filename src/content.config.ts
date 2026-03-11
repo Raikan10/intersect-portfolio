f1e2d95b859c4363aa3ab86284dcecc8a6d0731a
Ajay Rajnikanth <ajay.rajnikanth@gmail.com>
2026-03-12 00:19:47 +0100

    content shown

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,8 @@
 import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
 
 const graph = defineCollection({
-  type: "content",
+  loader: glob({ base: "./src/content/graph", pattern: "**/*.md" }),
   schema: z.object({
     title: z.string(),
     summary: z.string(),
